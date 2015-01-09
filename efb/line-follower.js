e860d4a1aa2e1d328e329955f85cbf1f6f83164b
HE Qichen <heqichen@gmail.com>
2015-01-10 03:21:10 +0800

    now can find all serial port

--- efb/line-follower.js
+++ efb/line-follower.js
@@ -1,12 +1,37 @@
-var serialPort = require("serialport");
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort;
 
-serialPort.list(function(err, ports){
+var efb = {
+	getAllSerialPortName: function(callback){
+		serialport.list(function(err, ports){
+			var serialPortNameList = [];
+			ports.forEach(function(port){
+				serialPortNameList[serialPortNameList.length] = port.comName;
+			});
 
-	console.log(ports.length + " port(s) found.");
+			callback(err, serialPortNameList);
+		});
+	}, 
 
-	ports.forEach(function(port){
-		console.log(port.comName);
-		console.log(port.pnpId);
-		console.log(port.manufacturer);
-	});
+	
+};
+
+
+efb.getAllSerialPortName(function(err, namelist){
+	console.log(namelist.length + " serial port(s) found.");
+
+	console.log(namelist);
 });
+
+
+/*
+var s0 = new SerialPort("/dev/ttyUSB0", {baudrate: 9600}, true);
+s0.open(function(err){
+	if (err) {
+		console.log("failed to open ");
+	} else {
+		console.log("usb0 opened");
+	}
+});
+
+*/
