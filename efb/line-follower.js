37c98a93e4a9e76da40f120274e25345b72891da
HE Qichen <heqichen@gmail.com>
2015-01-13 01:39:20 +0800

    now can write to serialport, but no receive data

--- efb/line-follower.js
+++ efb/line-follower.js
@@ -1,6 +1,25 @@
 var serialportLib = require("serialport");
 var SerialPort = serialportLib.SerialPort;
 
+
+var myParser = function(emitter, buffer) {
+  // Inspect buffer, emit on emitter:
+  console.log(buffer);
+  if(buffer.toString("utf8", 0, 3) === "foo")
+    emitter.emit("data", buffer);
+  else
+    emitter.emit("data", buffer);
+};
+
+var DEFAULT_SERIAL_OPTION = {
+	baudrate: 9600,
+	databits: 8,
+	parity: "none", 
+	stopbits: 1,
+	flowcontrol : false,
+	parser: myParser, 
+};
+
 var efb = {
 	getAllSerialPortName: function(callback){
 		serialportLib.list(function(err, ports){
@@ -21,30 +40,58 @@ var efb = {
 	openSerialPort: function(serialPortNameList, callback){
 		serialportList = [];
 		serialPortNameList.forEach(function(portName){
-			serialport = new SerialPort(portName, {baudrate:9600}, false);
+			serialport = new SerialPort(portName, DEFAULT_SERIAL_OPTION, false);
 			serialportList[serialportList.length] = serialport;
 		});
 		callback(null, serialportList);
 	}, 
 
 	setupSerialPort: function(serialportList, callback){
+		var tmpSpList = [];
+
 		serialportList.forEach(function(sp){
+
+			
+
 			sp.on("open", function(err){
 				if (err) {
-					console.log(sp.options.path  + " " + err.toString());
+					console.log(sp.path  + " " + err.toString());
 				} else {
-					console.log(sp.options.path + " opened");
+					console.log(sp.path + " opened, ready to write something to this port");
+					sp.on("data", function(data){
+						console.log("rrrr");
+						console.log(sp.path + " : receive data: " + data.toString());
+
+					});
+					sp.write("?", function(err, results){
+						if (err) {
+							console.log(sp.path + " error: " + err);
+						} else {
+							console.log(sp.path + " results: " + results);
+						}
+						
+						
+					});
+
 				}
+
 			});
 
-			sp.on("data", function(data){
-					console.log(sp.options.path + " : receive data: " + data.toString());
-			});
+
 
 			sp.open(function(err){
 
 			});
+
 		});
+
+
+
+/*		
+			
+		});
+*/
+
 	}, 
 };
 
