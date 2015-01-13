9e7a723bcfcfe95c98a09a63f9fa12a8773c57fd
HE Qichen <heqichen@gmail.com>
2015-01-14 01:35:07 +0800

    now can list all gadgets from http

--- efb/line-follower.js
+++ efb/line-follower.js
@@ -4,6 +4,8 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var async = require("async");
+var http = require("http");
+var url = require("url");
 
 var GADGET_TYPE_NOT_GADGET = 0;
 var GADGET_TYPE_LDR = 1;
@@ -22,55 +24,93 @@ var DEFAULT_SERIAL_OPTION = {
 	flowcontrol : false,
 };
 
+var gadgetList = [];
+
+var Gadget = function(sp, gadgetType) {
+	this._sp = sp;
+	this._gadgetType = gadgetType;
+};
+
+var addGadget = function(g) {
+	gadgetList[gadgetList.length] = g;
+}
+
 serialport.list(function(err, ports){
-  ports.forEach(function(port){
-    var sp = new SerialPort(port.comName, {baudrate:9600}, false);
-    var candidate = {
-    	spObj: sp,
-    	isGadget: false,
-    	gadgetType: GADGET_TYPE_NOT_GADGET, 
-    };
-    sp.on("open", function(){
-      console.log(sp.path + " opened");
-      
-      var writeRequest = function(){
-        sp.write("?");
-      };
-      var checkResponsePacket = function(data) {
-      	var dataStr = data.toString();
-      	if (dataStr.length === 1) {
-      		if (dataStr === "L") {
-      			candidate.isGadget = true;
-      			candidate.gadgetType = GADGET_TYPE_LDR;
-      		} else {
-      			if (dataStr === "M") {
-      				candidate.isGadget = true;
-      				candidate.gadgetType = GADGET_TYPE_DUAL_MOTOR;
-      			}
-      		}
-      	}
-      };
+	ports.forEach(function(port){
+		var sp = new SerialPort(port.comName, {baudrate:9600}, false);
+		var candidate = {
+			spObj: sp,
+			isGadget: false,
+			gadgetType: GADGET_TYPE_NOT_GADGET, 
+		};
+		sp.on("open", function(){
+			console.log(sp.path + " opened");
+			
+			var writeRequest = function(){
+				sp.write("?");
+			};
+			var checkResponsePacket = function(data) {
+				var dataStr = data.toString();
+				if (dataStr.length === 1) {
+					if (dataStr === "L") {
+						candidate.isGadget = true;
+						candidate.gadgetType = GADGET_TYPE_LDR;
+					} else {
+						if (dataStr === "M") {
+							candidate.isGadget = true;
+							candidate.gadgetType = GADGET_TYPE_DUAL_MOTOR;
+						}
+					}
+				}
+			};
 
-      sp.on("data", function(data){
-        checkResponsePacket(data);
-      });
+			sp.on("data", function(data){
+				checkResponsePacket(data);
+			});
+			//wait auto restart
+			setTimeout(writeRequest, 2000);
 
-      setTimeout(writeRequest, 2000);
+			var checkGadget = function()
+			{
+				if (candidate.isGadget) {
+					console.log(candidate.spObj.path + " is gadget : " + GADGET_TYPE_STR[candidate.gadgetType]);
+					var g = new Gadget(candidate.spObj, candidate.gadgetType);
+					addGadget(g);
+				}
+				else
+				{
+					candidate.spObj.close();
+				}
+			};
 
-      var checkGadget = function()
-      {
-      	if (candidate.isGadget) {
-      		console.log(candidate.spObj.path + " is gadget : " + GADGET_TYPE_STR[candidate.gadgetType]);
-      	}
-      	else
-      	{
-      		candidate.spObj.close();
-      	}
-      };
+			setTimeout(checkGadget, 3000);
 
-      setTimeout(checkGadget, 3000);
-
-    });
-    sp.open();
-  })
+		});
+		sp.open();
+	})
 });
+
+
+var server = http.createServer(function(req, resp) {
+	var urlObj = url.parse(req.url, true);
+	var pathname = urlObj.pathname;
+
+	resp.writeHead(200, { 'Content-Type': 'application/json' });
+	if (pathname === "/list_all_gadgets")
+	{
+		var respList = [];
+		gadgetList.forEach(function(gadget) {
+			var respObj = {
+				dev: gadget._sp.path, 
+        gadgetType: gadget._gadgetType, 
+        gadgetTypeStr: GADGET_TYPE_STR[gadget._gadgetType], 
+			};
+      respList[respList.length] = respObj;
+		});
+    resp.write(JSON.stringify(respList));
+    resp.end();
+
+	}
+});
+
+server.listen(8080);
